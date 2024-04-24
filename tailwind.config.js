e5f29efa6be9833ec5f834ab9e739f9c1f387f92
VaexuDa <vaexu.da@gmail.com>
2024-04-25 01:17:24 +0800

    feat(ui): add first component -> button

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./src/renderer/src/index.html', './src/renderer/src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        primary: {
+          DEFAULT: 'var(--primary)',
+          foreground: 'var(--primary-foreground)',
+          hover: 'var(--primary-hover)',
+          active: 'var(--primary-active)'
+        }
+      }
+    }
   },
   plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')]
 }
